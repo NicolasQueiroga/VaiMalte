ef01763c700888c5d9491408b26b661e46a8e736
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-03-15 20:30:30 -0300

    classe Caixa.java finalizada

--- src/br/edu/insper/desagil/vaimalte/Caixa.java
+++ src/br/edu/insper/desagil/vaimalte/Caixa.java
@@ -1,6 +1,34 @@
 package br.edu.insper.desagil.vaimalte;
 
+import java.util.*;
+
 public class Caixa 
 {
+	
+	private Map<Integer, Integer> descontos;
 
+	public Caixa() 
+	{
+		super();
+		this.descontos = new HashMap<>();
+	}
+	
+	public void adicionaDisconto(Produto produto, int desconto) 
+	{
+		if ((desconto >= 1) && (desconto <= 99))
+		{
+			descontos.put(produto.getCodigo(), desconto);
+		}
+	}
+	
+	public double calculaTotal(Carrinho carrinho)
+	{
+		double total = 0;
+		for (Pedido item : carrinho.getPedidos())
+		{
+			total += item.calculaPedido();
+		}
+		return total;
+	}
+	
 }
