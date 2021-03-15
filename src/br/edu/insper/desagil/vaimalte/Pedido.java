2a2e85d42af304dffd946ca9bbf14407275f376f
fran-janela <francisco.pinheiro.janela@gmail.com>
2021-03-15 18:46:44 -0300

    programa Pedido.java feito

--- src/br/edu/insper/desagil/vaimalte/Pedido.java
+++ src/br/edu/insper/desagil/vaimalte/Pedido.java
@@ -2,5 +2,27 @@ package br.edu.insper.desagil.vaimalte;
 
 public class Pedido 
 {
+	private Produto produto;
+	private int quantidade;
+	
+	public Pedido(Produto produto) {
+		this.produto = produto;
+		this.quantidade = 1;
+	}
 
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void incrementaUm() {
+		this.quantidade ++;
+	}
+	public double calculaPedido() {
+		double pedido = this.produto.getPreco() * this.quantidade;
+		return pedido;
+	}
 }
